076fef538f43d31acde2d75cbf50b061588caf35
tyoukensen <tyoukensen@gmail.com>
2020-02-02 22:05:15 +0800

    增加資料存入文字檔功能，修改104程式碼，解決某些關鍵字會出現閃退的情形。

--- 104.py
+++ 104.py
@@ -8,18 +8,23 @@ Created on Sat Feb  1 14:04:55 2020
 import requests
 from bs4 import BeautifulSoup
 
-xx="tensorflow" #搜尋工作類型
-yy=10       #頁數
-for i in range(1,yy):
+xx="高薪" #搜尋工作類型       
+
+for i in range(1,10):
     url=f"https://www.104.com.tw/jobs/search/?ro=0&kwop=7&keyword={xx}&area=6001008000&order=11&asc=0&page={i}&mode=s"
     aa=requests.get(url)
     bs=BeautifulSoup(aa.text, 'html.parser')
     ww=bs.find_all(class_="b-block--top-bord job-list-item b-clearfix js-job-item")
-    for xx in ww:
-        cc=xx.find(class_="b-tit")
-        yy=xx.find(class_="job-list-item__info b-clearfix b-content")
+    for dd in ww:
+        cc=dd.find(class_="b-tit")
+        ee=dd.find(class_="b-block__left")
         print(cc.a.text.strip())
         print(cc.a["href"])
-        print(yy.text.strip())
+        try:
+            
+            print(ee.p.text.strip())
+            
+        except BaseException as k:
+            print(k)
         print("---------------------------------------------------")
         
--- main.py
+++ main.py
@@ -8,25 +8,38 @@ import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
-
-
+work_name="python"
+f = open("work.txt", "w",encoding="utf-8")
 
 def w104(xx):
-    yy=10       #頁數
-    for i in range(1,yy):
+          #頁數
+    print(f"{w104.__name__:=^160}",file=f)
+    for i in range(1,10):
         url=f"https://www.104.com.tw/jobs/search/?ro=0&kwop=7&keyword={xx}&area=6001008000&order=11&asc=0&page={i}&mode=s"
         aa=requests.get(url)
         bs=BeautifulSoup(aa.text, 'html.parser')
         ww=bs.find_all(class_="b-block--top-bord job-list-item b-clearfix js-job-item")
-        for xx in ww:
-            cc=xx.find(class_="b-tit")
-            yy=xx.find(class_="job-list-item__info b-clearfix b-content")
-            print(cc.a.text.strip())
-            print(cc.a["href"])
-            print(yy.text.strip())
-            print("---------------------------------------------------")
+        
+       
+        for dd in ww:
+            cc=dd.find(class_="b-tit")
+            yy=dd.find(class_="job-list-item__info b-clearfix b-content")
+            
+          
+            
+            print(cc.a.text.strip(),file=f)
+            print(cc.a["href"],file=f)
+            try:
+                
+                print(yy.text.strip(),file=f)
+                
+            except BaseException as k:
+                print(k)
+            print("---------------------------------------------------",file=f)
+          
 #    zz="本函式結束"
-    print(f"{w104.__name__:=^40}")
+#    print(f"{w104.__name__:=^80}",file=f)
+    
     
 
 def w1111(xx):
@@ -35,13 +48,15 @@ def w1111(xx):
     cc=BeautifulSoup(aa.text,"html.parser")
     dd=cc.find_all("div","jbInfoin")
     ff=cc.find_all("div","jbInfoTxt")
+    print(f"{w1111.__name__:=^160}",file=f)
+
     for ee,gg in zip(dd,ff):
-        print(ee.h3.a["title"]) 
-        print(ee.h3.a["href"])
-        print(gg.text)
-        print("-"*80)
+        print(ee.h3.a["title"],file=f) 
+        print(ee.h3.a["href"],file=f)
+        print(gg.text,file=f)
+        print("-"*80,file=f)
         
-    print(f"{w1111.__name__:=^40}")
+#    print(f"{w1111.__name__:=^80}",file=f)
 
 
 def w518(xx):
@@ -50,13 +65,14 @@ def w518(xx):
     bb=BeautifulSoup(bb.text,"html.parser")
     cc=bb.find_all(class_="title")
     dd=bb.find_all(class_="sumbox")
+    print(f"{w518.__name__:=^160}",file=f)
 
     for ee,gg in zip(cc,dd):
-        print(ee.text.strip())
-        print(ee.a["href"])
-        print(gg.text.strip())
-        print("-"*85)
-    print(f"{w518.__name__:=^40}")
+        print(ee.text.strip(),file=f)
+        print(ee.a["href"],file=f)
+        print(gg.text.strip(),file=f)
+        print("-"*85,file=f)
+#    print(f"{w518.__name__:=^80}",file=f)
 
 
 
@@ -72,14 +88,15 @@ def yes123(xx):
     aa=dr.find_elements_by_class_name("t0")
     cc=dr.find_elements_by_xpath("//span[@class='t0']//a")
     ee=dr.find_elements_by_class_name("t3")
+    print(f"{yes123.__name__:=^160}",file=f)
     for bb,dd,ff in zip(aa,cc,ee):
-        print(bb.text)
-        print(dd.get_attribute("href"))
-        print(ff.text.strip())
-        print("-"*85)
+        print(bb.text,file=f)
+        print(dd.get_attribute("href"),file=f)
+        print(ff.text.strip(),file=f)
+        print("-"*85,file=f)
     
     dr.quit()
-    print(f"{yes123.__name__:=^40}")
+#    print(f"{yes123.__name__:=^80}",file=f)
     
 
 
@@ -87,14 +104,22 @@ def yes123(xx):
 
 
 def main(xx):
-
+    
+   
     
     w104(xx)
     w1111(xx)
     w518(xx)
     yes123(xx)
     
+    
+    
+    
+    
 if __name__ == '__main__':
     
     
-    main("python")
+    main(work_name)
+    
+f.close()
+print("end")
