0d721c2e12c1942d550b87c44962b72b7055ebca
tyoukensen <tyoukensen@gmail.com>
2020-11-02 09:44:24 +0800

    修改文檔

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
-#瀏覽器限定chrome
+# 抓取人力銀行爬蟲
+## 注意事項
+* 瀏覽器限定chrome
 
-#在main.py裡面，修改main（XX）變數，可一次抓取四大網站資料。
+* 在main.py裡面，修改main（XX）變數，可一次抓取四大網站資料。
 
-#計劃新增網頁版，APP版本，自存檔案功能，傳送line通知
+* 計劃新增網頁版，APP版本，自存檔案功能，傳送line通知
 
-#104.py 1111.py 518.py yes13.py 可獨立抓取網站，修改XX變數即可。
+* 104.py 1111.py 518.py yes13.py 可獨立抓取網站，修改XX變數即可。
+* 計畫將抓下來的資料，轉成json檔案，讓vue顯示出來
+
+* chromedriver.exe需安裝後才能使用yes123.py
+* 執行main.py 結果會顯示在 work.txt
+  
--- main.py
+++ main.py
@@ -8,118 +8,107 @@ import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
-work_name="python"
-f = open("work.txt", "w",encoding="utf-8")
+work_name = "python"
+f = open("work.txt", "w", encoding="utf-8")
+
+# 測試
+
 
 def w104(xx):
-          #頁數
-    print(f"{w104.__name__:=^160}",file=f)
-    for i in range(1,10):
-        url=f"https://www.104.com.tw/jobs/search/?ro=0&kwop=7&keyword={xx}&area=6001008000&order=11&asc=0&page={i}&mode=s"
-        aa=requests.get(url)
-        bs=BeautifulSoup(aa.text, 'html.parser')
-        ww=bs.find_all(class_="b-block--top-bord job-list-item b-clearfix js-job-item")
-        
-       
+    # 頁數
+    print(f"{w104.__name__:=^160}", file=f)
+    for i in range(1, 10):
+        url = f"https://www.104.com.tw/jobs/search/?ro=0&kwop=7&keyword={xx}&area=6001008000&order=11&asc=0&page={i}&mode=s"
+        aa = requests.get(url)
+        bs = BeautifulSoup(aa.text, 'html.parser')
+        ww = bs.find_all(
+            class_="b-block--top-bord job-list-item b-clearfix js-job-item")
+
         for dd in ww:
-            cc=dd.find(class_="b-tit")
-            yy=dd.find(class_="job-list-item__info b-clearfix b-content")
-            
-          
-            
-            print(cc.a.text.strip(),file=f)
-            print(cc.a["href"],file=f)
+            cc = dd.find(class_="b-tit")
+            yy = dd.find(class_="job-list-item__info b-clearfix b-content")
+
+            print(cc.a.text.strip(), file=f)
+            print(cc.a["href"], file=f)
             try:
-                
-                print(yy.text.strip(),file=f)
-                
+
+                print(yy.text.strip(), file=f)
+
             except BaseException as k:
                 print(k)
-            print("---------------------------------------------------",file=f)
-          
+            print("---------------------------------------------------", file=f)
+
 #    zz="本函式結束"
 #    print(f"{w104.__name__:=^80}",file=f)
-    
-    
+
 
 def w1111(xx):
-    url=f"https://www.1111.com.tw/job-bank/job-index.asp?si=1&ks={xx}&c0=100900&fs=1&col=da&sort=desc&page=1"
-    aa=requests.get(url)
-    cc=BeautifulSoup(aa.text,"html.parser")
-    dd=cc.find_all("div","jbInfoin")
-    ff=cc.find_all("div","jbInfoTxt")
-    print(f"{w1111.__name__:=^160}",file=f)
+    url = f"https://www.1111.com.tw/job-bank/job-index.asp?si=1&ks={xx}&c0=100900&fs=1&col=da&sort=desc&page=1"
+    aa = requests.get(url)
+    cc = BeautifulSoup(aa.text, "html.parser")
+    dd = cc.find_all("div", "jbInfoin")
+    ff = cc.find_all("div", "jbInfoTxt")
+    print(f"{w1111.__name__:=^160}", file=f)
 
-    for ee,gg in zip(dd,ff):
-        print(ee.h3.a["title"],file=f) 
-        print(ee.h3.a["href"],file=f)
-        print(gg.text,file=f)
-        print("-"*80,file=f)
-        
+    for ee, gg in zip(dd, ff):
+        print(ee.h3.a["title"], file=f)
+        print(ee.h3.a["href"], file=f)
+        print(gg.text, file=f)
+        print("-"*80, file=f)
+
 #    print(f"{w1111.__name__:=^80}",file=f)
 
 
 def w518(xx):
-    url=f"https://www.518.com.tw/job-index-P-1.html?i=1&am=1&ad={xx}&ai=0&orderType=1&orderField=8"
-    bb=requests.get(url)
-    bb=BeautifulSoup(bb.text,"html.parser")
-    cc=bb.find_all(class_="title")
-    dd=bb.find_all(class_="sumbox")
-    print(f"{w518.__name__:=^160}",file=f)
+    url = f"https://www.518.com.tw/job-index-P-1.html?i=1&am=1&ad={xx}&ai=0&orderType=1&orderField=8"
+    bb = requests.get(url)
+    bb = BeautifulSoup(bb.text, "html.parser")
+    cc = bb.find_all(class_="title")
+    dd = bb.find_all(class_="sumbox")
+    print(f"{w518.__name__:=^160}", file=f)
 
-    for ee,gg in zip(cc,dd):
-        print(ee.text.strip(),file=f)
-        print(ee.a["href"],file=f)
-        print(gg.text.strip(),file=f)
-        print("-"*85,file=f)
+    for ee, gg in zip(cc, dd):
+        print(ee.text.strip(), file=f)
+        print(ee.a["href"], file=f)
+        print(gg.text.strip(), file=f)
+        print("-"*85, file=f)
 #    print(f"{w518.__name__:=^80}",file=f)
 
 
-
 def yes123(xx):
-    url="https://www.yes123.com.tw/admin/job_refer_list.asp"
+    url = "https://www.yes123.com.tw/admin/job_refer_list.asp"
 #    xx="python"
-    dr=webdriver.Chrome()
+    dr = webdriver.Chrome()
     dr.get(url)
     dr.find_element_by_id("find_key1").send_keys(xx)
     dr.find_element_by_class_name("bt_search").click()
     dr.find_element_by_xpath("//div[@class='illustrate']//li[2]//a[1]").click()
     dr.find_element_by_xpath("//li[@class='date2']//a").click()
-    aa=dr.find_elements_by_class_name("t0")
-    cc=dr.find_elements_by_xpath("//span[@class='t0']//a")
-    ee=dr.find_elements_by_class_name("t3")
-    print(f"{yes123.__name__:=^160}",file=f)
-    for bb,dd,ff in zip(aa,cc,ee):
-        print(bb.text,file=f)
-        print(dd.get_attribute("href"),file=f)
-        print(ff.text.strip(),file=f)
-        print("-"*85,file=f)
-    
+    aa = dr.find_elements_by_class_name("t0")
+    cc = dr.find_elements_by_xpath("//span[@class='t0']//a")
+    ee = dr.find_elements_by_class_name("t3")
+    print(f"{yes123.__name__:=^160}", file=f)
+    for bb, dd, ff in zip(aa, cc, ee):
+        print(bb.text, file=f)
+        print(dd.get_attribute("href"), file=f)
+        print(ff.text.strip(), file=f)
+        print("-"*85, file=f)
+
     dr.quit()
 #    print(f"{yes123.__name__:=^80}",file=f)
-    
-
-
-
 
 
 def main(xx):
-    
-   
-    
+
     w104(xx)
     w1111(xx)
     w518(xx)
     yes123(xx)
-    
-    
-    
-    
-    
+
+
 if __name__ == '__main__':
-    
-    
+
     main(work_name)
-    
+
 f.close()
 print("程式結束，資料儲存在work文件檔裡面")
